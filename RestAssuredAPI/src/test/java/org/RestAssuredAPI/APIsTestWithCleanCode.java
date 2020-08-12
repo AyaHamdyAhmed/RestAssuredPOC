5ca666e57195af729dcc9a7614602d88f23b4769
AyaHamdyAhmed <46898899+AyaHamdyAhmed@users.noreply.github.com>
2020-08-12 14:42:28 +0200

    adding extract mechanism to compare API response with DB later

---
+++ RestAssuredAPI/src/test/java/org/RestAssuredAPI/APIsTestWithCleanCode.java
@@ -0,0 +1,74 @@
+package org.RestAssuredAPI;
+
+import org.testng.annotations.Test;
+import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.builder.ResponseSpecBuilder;
+import io.restassured.http.ContentType;
+import io.restassured.specification.RequestSpecification;
+import io.restassured.specification.ResponseSpecification;
+import static org.hamcrest.Matchers.equalTo;
+import static io.restassured.RestAssured.given;
+
+public class APIsTestWithCleanCode {
+
+	private static RequestSpecification reqSpec;
+	
+  @BeforeClass
+  private static void createRequestSpecs() {
+	  reqSpec = new RequestSpecBuilder().setBaseUri("http://zippopotam.us").build();
+  }
+
+  @Test
+  public void CheckReqestStatusCode() {
+	given().
+     spec(reqSpec).
+    when().
+     get("us/90210").
+    then().
+     assertThat().
+      statusCode(200);
+   }
+
+  private static ResponseSpecification responseSpec;
+
+  @BeforeClass
+  public static void createResponseSpecification() {
+
+      responseSpec = new ResponseSpecBuilder().
+          expectStatusCode(200).
+          expectContentType(ContentType.JSON).
+          build();
+  }
+
+  @Test
+  public void checkPlaceNameInResponseBody_withResponseSpec() {
+
+      given().
+          spec(reqSpec).
+      when().
+          get("http://zippopotam.us/us/90210").
+      then().
+          spec(responseSpec).
+      and().
+          assertThat().
+          body("places[0].'place name'", equalTo("Beverly Hills"));
+  }
+
+  @Test
+  public void extractPlaceNameFromResponseBody() {
+
+      String placeName =
+
+      given().
+          spec(reqSpec).
+      when().
+          get("http://zippopotam.us/us/90210").
+      then().
+          extract().
+          path("places[0].'place name'");
+      // here after the API response extraction i can compare it with the DB values after opening a connection with it
+      Assert.assertEquals("Beverly Hills", placeName);
+}
+}
--- RestAssuredAPI/src/test/java/org/RestAssuredAPI/ApiTest1NG.java
+++ RestAssuredAPI/src/test/java/org/RestAssuredAPI/ApiTest1NG.java
@@ -4,8 +4,6 @@ import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.*;
 
 import classes.API;
-
-import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 public class ApiTest1NG {
